5eb5488e820b707e52a18064bc4be0862c675a64
Juho Viljamaa <viljamaa@student.tut.fi>
2018-09-14 17:31:33 +0300

    4th version of 3.3.3, changes to class 'Square': Implemented 'addFlag', 'removeFlag' and 'hasFlag' methods. Fixed 'print' method to work with the new methods. Added comments.

--- student/03/minesweeper/square.cpp
+++ student/03/minesweeper/square.cpp
@@ -7,9 +7,9 @@ Square::Square(int x, int y, bool hasMine, std::vector< std::vector< Square > >*
 
 bool Square::hasFlag() const
 {
-    return false;
+    return hasFlag_;
 }
-// delet this
+// delet this, do not change
 bool Square::hasMine() const
 {
     return hasMine_;
@@ -26,6 +26,7 @@ bool Square::isReady() const
     return false;
 }
 
+// Returns int values from 0 to the size of board, paramteter values out of this range are changed to edge values of this range.
 unsigned int Square::sizeVal(int size_int) {
 
     int board_size = board_->size();
@@ -40,34 +41,40 @@ unsigned int Square::sizeVal(int size_int) {
     }
 }
 
+// Determines output mark of this square to parameter stream.
 void Square::print(std::ostream& stream) const
 {
-    if (hasMine_) {
-        stream << ".";
-
-    } else if (hasFlag_) {
+    if (hasFlag_) {
         stream << "P";
+
+    } else if (hasMine_) {
+        stream << ".";
 
     } else {
         stream << adMines_;
     }
 }
 
+// Removes flag from this square.
 void Square::removeFlag()
 {
-
+    hasFlag_ = false;
 }
 
+// Adds flag to this square.
 void Square::addFlag()
 {
-
+    hasFlag_ = true;
 }
 
+// Counts how many mines are there in squares adjacent to this square.
 void Square::countAdjacent()
 {
+    // using sizeVal to prevent index errors
     for (unsigned int y = sizeVal(y_ - 1) ; y < sizeVal(y_ + 2) ; ++y) {
 
         for (unsigned int x = sizeVal(x_ - 1) ; x < sizeVal(x_ + 2) ; ++x) {
+
             Square square = board_->at(y).at(x);
             if (square.hasMine_) {
                 adMines_ += 1;
