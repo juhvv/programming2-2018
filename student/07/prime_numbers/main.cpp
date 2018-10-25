9d89181f31471cdf54e920b86a78ab0eb65a76ab
Juho Viljamaa <viljamaa@student.tut.fi>
2018-10-25 18:20:11 +0300

    Assignment 7.3.2 second submission. Added missing print line.

--- student/07/prime_numbers/main.cpp
+++ student/07/prime_numbers/main.cpp
@@ -16,7 +16,7 @@ int main()
     while ( num < ubound ) {
         if (num == 0 or num == 1) {
             *primePtr = false;
-        } else if ((num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0) and (num != 2 and num != 3 and num != 5 and num != 7)) {
+        } else if ((num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0 or num % 11 == 0) and (num != 2 and num != 3 and num != 5 and num != 7 and num != 11)) {
             *primePtr = false;
         } else {
             *primePtr = true;
@@ -27,6 +27,7 @@ int main()
         ++num;
     }
 
+    std::cout << "Prime numbers smaller than " << ubound << ":" << std::endl;
     int index = 0;
     while (index < ubound) {
         if (primes[index]) {
