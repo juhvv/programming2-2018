9d23ac4393bc3c552128e9f609a3b38d920d68fb
Juho Viljamaa <viljamaa@student.tut.fi>
2018-10-25 18:16:35 +0300

    Assignment 7.3.2 first submission.

---
+++ student/07/prime_numbers/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    int ubound;
+    std::cout << "Enter an upper bound: ";
+    std::cin >> ubound;
+
+    bool* primePtr = nullptr;
+    bool primes[ubound];
+    primePtr = primes;
+
+    int num = 0;
+    while ( num < ubound ) {
+        if (num == 0 or num == 1) {
+            *primePtr = false;
+        } else if ((num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0) and (num != 2 and num != 3 and num != 5 and num != 7)) {
+            *primePtr = false;
+        } else {
+            *primePtr = true;
+        }
+
+
+        ++primePtr;
+        ++num;
+    }
+
+    int index = 0;
+    while (index < ubound) {
+        if (primes[index]) {
+            std::cout << index << " ";
+        }
+        ++index;
+    }
+    std::cout << std::endl;
+
+}
